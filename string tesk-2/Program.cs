9cecc3cd64bb7356a83dcee4628475d241041a47
zuxriddinCsharp <137428899+zuxriddinCsharp@users.noreply.github.com>
2023-07-19 22:04:56 +0500

    Добавьте файлы проекта.

---
+++ string tesk-2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace string_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your full name: ");
+            string fullName = Console.ReadLine();
+
+            string[] name = fullName.Split(' ');
+
+            char[] chars = name[0].ToCharArray();
+
+            char[] chars1 = name[1].ToCharArray();
+
+            Console.WriteLine($"Result:  {chars[0]}.{chars1[0]}.{name[2]}");
+        }
+    }
+}
